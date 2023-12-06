e289b2eed44cf98eb3afcbe001d5253ba5023715
Sori Kim <73383923+sorikikikim@users.noreply.github.com>
2023-12-07 00:31:09 +0900

    :art: Style: css to scss
    Style: css to scss

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,27 @@
 import Navbar from 'components/Layout/Navbar';
 import React from 'react';
 
-interface HomePageProps {}
+// interface HomePageProps {}
 
-function HomePage(): React.FC<HomePageProps> {
+// function HomePage(): React.FC<HomePageProps> {
+// 	return (
+// 		<div className="components-wrapper">
+// 			{/* <div className='header-wrapper'>Header 자리</div>
+//       <div className='main-wrapper'>내용물 자리</div>
+//       <div className='navbar-wrapper'>Navbar 자리</div> */}
+// 			<div>Header 자리</div>
+// 			<div>내용물 자리</div>
+// 			<Navbar />
+// 		</div>
+// 	);
+// }
+
+export default function HomePage() {
 	return (
 		<div className="components-wrapper">
-			{/* <div className='header-wrapper'>Header 자리</div>
-      <div className='main-wrapper'>내용물 자리</div>
-      <div className='navbar-wrapper'>Navbar 자리</div> */}
-			<div>Header 자리</div>
-			<div>내용물 자리</div>
+			<div className="header-wrapper">Header 자리</div>
+			<div className="main-wrapper">내용물 자리</div>
 			<Navbar />
 		</div>
 	);
 }
-export default HomePage;
