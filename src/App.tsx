45e7b15845d24ec2c4024f3f8642e8cb963c7202
dongsu.han <kku304@naver.com>
2023-12-12 15:26:19 +0900

    🐛fix/35-eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,6 @@ import './App.scss';
 import './styles/Layout.scss';
 import Router from 'routes/Router';
 
-const locationList = [
-	{
-		location: '내 위치',
-		time: '인천광역시 연수구',
-		tempInfo: { temperature: -2, lowestTemperature: -8, highestTemperature: 24 },
-		explain: '대체로 청명함',
-	},
-];
-
 function App() {
 	return <Router />;
 }
