a64e724b901310461c4856f37564f85d3cc80362
dongsu.han <kku304@naver.com>
2023-12-14 20:32:29 +0900

    ✨feature/44-todayComponent

--- src/components/Today/TodayCard.tsx
+++ src/components/Today/TodayCard.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
-// import cludyDark from '../../assets/weather/cloudy-dark.png';
+import React, { useState } from 'react';
+import './TodayCard.scss';
+import clody from '../../assets/weather/cloudy-dark.png';
+import windy from '../../assets/weather/windy-dark.png';
+import snow from '../../assets/weather/snow-dark.png';
+import rainy from '../../assets/weather/rainy-dark.png';
+import sunny from '../../assets/weather/sunny-dark.png';
 
-export default function TodayCard() {
+const getWeatherIcon = (condition: string): string => {
+	switch (condition) {
+		case '맑음':
+			return sunny;
+		case '흐림':
+			return clody;
+		case '비':
+			return rainy;
+		case '눈':
+			return snow;
+		case '바람':
+			return windy;
+		default:
+			return '';
+	}
+};
+
+export interface HourlyWeatherInfo {
+	time: string;
+	tempInfo: {
+		temperature: number;
+	};
+	condition: string;
+}
+
+export interface TodayCardProps {
+	hourlyWeather: HourlyWeatherInfo[];
+	explain: string;
+}
+
+export default function TodayCard({ hourlyWeather, explain }: TodayCardProps) {
 	return (
-		<div className="container">
-			<p className="explain" />
+		<div className="today-container">
+			<p className="today-explain">{explain}</p>
+			<section className="hour-section">
+				{hourlyWeather.map((hour, index) => (
+					<div className="hour-box" key={index}>
+						<img src={getWeatherIcon(hour.condition)} alt="hourly icon" />
+						<p className="temp">{hour.tempInfo.temperature}°</p>
+						<p className="time">{hour.time}</p>
+					</div>
+				))}
+			</section>
 		</div>
 	);
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,8 +3,7 @@ import Header from 'components/Header/Header';
 import React from 'react';
 // import LoginPage from 'pages/Login/LoginPage';
 import LocationCard from 'components/Cards/LocationCard';
-import WhiteCard from 'components/Today/WhiteCard';
-import './HomePage.scss';
+import TodayWeather from 'components/Today/TodayWeather';
 
 const CardProps = {
 	location: '충청북도 제천시',
@@ -14,15 +13,6 @@ const CardProps = {
 		feelTemperature: '20',
 	},
 	explain: '대체로 청명함',
-};
-
-const WhiteCardProps = {
-	tempInfo: {
-		feelTemperature: '-2',
-	},
-	precipitation: 0,
-	uv: 5,
-	humidity: 68,
 };
 
 export default function HomePage() {
@@ -37,36 +27,7 @@ export default function HomePage() {
 					explain={CardProps.explain}
 					tempInfo={CardProps.tempInfo}
 				/>
-				<section className="WhiteContainer">
-					<WhiteCard
-						title="체감 온도"
-						tempInfo={{ feelTemperature: Number(WhiteCardProps.tempInfo.feelTemperature) }}
-						precipitation={WhiteCardProps.precipitation}
-						uv={WhiteCardProps.uv}
-						humidity={WhiteCardProps.humidity}
-					/>
-					<WhiteCard
-						title="강수량"
-						tempInfo={{ feelTemperature: Number(WhiteCardProps.tempInfo.feelTemperature) }}
-						precipitation={WhiteCardProps.precipitation}
-						uv={WhiteCardProps.uv}
-						humidity={WhiteCardProps.humidity}
-					/>
-					<WhiteCard
-						title="자외선 지수"
-						tempInfo={{ feelTemperature: Number(WhiteCardProps.tempInfo.feelTemperature) }}
-						precipitation={WhiteCardProps.precipitation}
-						uv={WhiteCardProps.uv}
-						humidity={WhiteCardProps.humidity}
-					/>
-					<WhiteCard
-						title="습도"
-						tempInfo={{ feelTemperature: Number(WhiteCardProps.tempInfo.feelTemperature) }}
-						precipitation={WhiteCardProps.precipitation}
-						uv={WhiteCardProps.uv}
-						humidity={WhiteCardProps.humidity}
-					/>
-				</section>
+				<TodayWeather />
 			</div>
 			<Navbar page="home" />
 		</div>
